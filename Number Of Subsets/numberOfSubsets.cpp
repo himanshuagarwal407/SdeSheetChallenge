17d5217a09506efa01bddd1bdd13631b592778d8
Himanshu Agarwal <55655064+himanshuagarwal407@users.noreply.github.com>
2022-07-27 22:45:34 +0530

    Create numberOfSubsets.cpp

---
+++ Number Of Subsets/numberOfSubsets.cpp
@@ -0,0 +1,30 @@
+int solve(vector<int> num, int target, int ind, vector<vector<int>>& dp)
+{
+    if(ind==0)
+    {
+        if(target==0 && num[0]==0)
+            return 2;
+        if(target==0 || target==num[0])
+            return 1;
+        return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+    
+    int notTake = solve(num, target, ind-1, dp);
+    int take=0;
+    if(num[ind]<=target)
+        take = solve(num, target-num[ind], ind-1, dp);
+    
+    return dp[ind][target] = take+notTake;
+}
+
+int findWays(vector<int> &num, int tar)
+{
+    int n=num.size();
+    vector<vector<int>> dp(n, vector<int>(tar+1, -1));
+    
+    return solve(num, tar, n-1, dp);
+    // Write your code here.
+}
